5b0a9770f8da497b9e111227698e28b1e858bdd8
Edwin <kurniawan.edwin@me.com>
2017-02-18 21:37:44 +0700

    Get data

--- functions/sallary_func.php
+++ functions/sallary_func.php
@@ -9,31 +9,38 @@ function insert_sallary($nama_penghasilan, $tanggal_penghasilan, $nominal_pengha
   $nominal_penghasilan  = mysqli_real_escape_string($db_connect,$nominal_penghasilan);
 
   $query = "INSERT INTO penghasilan(id_user,nama_penghasilan,tanggal_penghasilan,
-  nominal_penghasilan) VALUES(1,'$nama_penghasilan','$tanggal_penghasilan','$nominal_penghasilan')";
+    nominal_penghasilan) VALUES(1,'$nama_penghasilan','$tanggal_penghasilan','$nominal_penghasilan')";
 
-  if ( mysqli_query($db_connect,$query) ) {
-    return true;
-  }else {
-    return false;
-  }
-
-}
-
-function retrieve_sallary(){
-  global $db_connect;
-
-  $query = "SELECT * FROM penghasilan ORDER BY tanggal_penghasilan DESC";
-
-  if ( $result = mysqli_query($db_connect,$query) ) {
-    if ( mysqli_num_rows($result) > 0 ) {
-      while ( $data = mysqli_fetch_assoc($result) ) {
-        echo $data['nama_penghasilan']." ".$data['tanggal_penghasilan']."<br>";
-      }
+    if ( mysqli_query($db_connect,$query) ) {
+      return true;
     }else {
       return false;
     }
+
   }
 
-}
+  function get_sallary(){
+    global $db_connect;
 
- ?>
+    $query = "SELECT * FROM penghasilan GROUP BY tanggal_penghasilan ORDER BY tanggal_penghasilan DESC";
+
+    if ( $result = mysqli_query($db_connect,$query) ) {
+
+      if ( mysqli_num_rows($result) > 0 ) {
+
+        while ( $data = mysqli_fetch_assoc($result) ) {
+          echo
+          "<tr>
+            <td>".$data['nama_penghasilan']."</td>
+            <td>".$data['tanggal_penghasilan']."</td>
+          </tr>
+          ";
+        }
+      }else {
+        return false;
+      }
+    }
+
+  }
+
+  ?>
--- view_sallary.php
+++ view_sallary.php
@@ -5,10 +5,27 @@ require_once "core/init.php";
 
 require_once "views/header.php";
 
-retrieve_sallary();
 ?>
 
+<div class="col-md-3"></div>
 
+<div class="col-md-6">
+
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Nama Penghasilan</th>
+        <th>Tanggal Penghasilan</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php get_sallary() ?>
+    </tbody>
+  </table>
+
+</div>
+
+<div class="col-md-3"></div>
 
 
 
