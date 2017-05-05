f75ad73805f3d93b4a3bcb2c144dae9cada6a076
AdrianEdwin <kurniawan.edwin@me.com>
2017-05-05 21:06:18 +0700

    View pengeluaran

--- view-pengeluaran.php
+++ view-pengeluaran.php
@@ -2,14 +2,18 @@
 include_once "classes/class.crud.pengeluaran.php";
 include_once "views/header.php";
 $pengeluaran = new Pengeluaran();
+$Pghsisset = false;
 
 if (isset($_POST['btn-add'])){
     if ($pengeluaran->Pghsisset($userRow['userId'])){
+        $Pghsisset = false;
         header("Location: add-pengeluaran.php");
     }else{
-        header("Location: index.php");
+        $Pghsisset = true;
+//        header("Location: index.php");
     }
 }
+
 
 
 ?>
@@ -19,23 +23,34 @@ if (isset($_POST['btn-add'])){
     <div class="container">
         <h1>Komponen Pengeluaran</h1>
         <hr>
+        <?php
+        if($Pghsisset){
+            ?>
+            <div class="container">
+                <div class="alert alert-danger">
+                    <strong>Terjadi kesalahan!</strong> Anda belum memasukkan data penghasilan pada bulan <?php echo date('F') ?>, silakan isi penghasilan Anda <a href="add-penghasilan.php">di sini</a>!
+                </div>
+            </div>
+            <?php
+        }
+        ?>
 
-    <form method="post">
-        <button class="btn btn-large btn-info" name="btn-add" type="submit"><i class="glyphicon glyphicon-plus"></i> &nbsp; Add Records </button>
+        <form method="post">
+            <button class="btn btn-large btn-info" name="btn-add" type="submit"><i class="glyphicon glyphicon-plus"></i> &nbsp; Add Records </button>
 
-    </form>
+        </form>
     </div>
 
     <div class="clearfix"></div><br />
 
     <div class="container">
 
-            <?php
-            $query = "SELECT * FROM kompPengeluaran WHERE flag='0' AND userId='".$userRow['userId']."'ORDER BY namaKomp ASC";
-            $records_per_page=5;
-            $newquery = $pengeluaran->paging($query,$records_per_page);
-            $pengeluaran->dataview($query);
-            ?>
+        <?php
+        $query = "SELECT * FROM kompPengeluaran WHERE flag='0' AND MONTH(tglKomp) = MONTH(CURRENT_DATE) AND userId='".$userRow['userId']."'ORDER BY namaKomp ASC";
+        $records_per_page=5;
+        $newquery = $pengeluaran->paging($query,$records_per_page);
+        $pengeluaran->dataview($query);
+        ?>
 
 
 
