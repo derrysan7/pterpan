bda84ce7ba1bd2a560d5b27199617a914049b782
AdrianEdwin <kurniawan.edwin@me.com>
2017-05-24 15:57:54 +0700

    Wrapping penghasilan

--- delete-penghasilan.php
+++ delete-penghasilan.php
@@ -1,5 +1,6 @@
 <?php
 require_once "classes/Penghasilan.php";
+$page=2;
 $penghasilan = new Penghasilan();
 
 if(isset($_POST['btn-del']))
@@ -12,87 +13,88 @@ if(isset($_POST['btn-del']))
 ?>
 
 <?php include_once 'views/header.php'; ?>
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+        <div class="clearfix"></div>
 
-    <div class="clearfix"></div>
+        <div class="container">
 
-    <div class="container">
-
-        <?php
-        if(isset($_GET['deleted']))
-        {
-            ?>
-            <div class="alert alert-success">
-                <strong>Success!</strong> record was deleted...
-            </div>
             <?php
-        }
-        else
-        {
-            ?>
-            <div class="alert alert-danger">
-                <strong>Sure !</strong> to remove the following record ?
-            </div>
-            <?php
-        }
-        ?>
-    </div>
-
-    <div class="clearfix"></div>
-
-    <div class="container">
-
-        <?php
-        if(isset($_GET['delete_id']))
-        {
-            ?>
-            <table class='table table-bordered'>
-                <tr>
-                    <th>#</th>
-                    <th>Sumber Penghasilan</th>
-                    <th>Tanggal Penghasilan</th>
-                    <th>Nominal Penghasilan</th>
-                </tr>
-                <?php
-                $stmt = $penghasilan->db->prepare("SELECT * FROM penghasilan WHERE penghasilanId=:id");
-                $stmt->execute(array(":id"=>$_GET['delete_id']));
-                while($row=$stmt->fetch(PDO::FETCH_BOTH))
-                {
-                    ?>
-                    <tr>
-                        <td><?php print($row['penghasilanId']); ?></td>
-                        <td><?php print($row['sumberPghs']); ?></td>
-                        <td><?php print($row['tglPghs']); ?></td>
-                        <td align="right"><?php print("Rp ".$row['nominalPghs']); ?></td>
-                    </tr>
-                    <?php
-                }
+            if(isset($_GET['deleted']))
+            {
                 ?>
-            </table>
-            <?php
-        }
-        ?>
-    </div>
+                <div class="alert alert-success">
+                    <strong>Success!</strong> record was deleted...
+                </div>
+                <?php
+            }
+            else
+            {
+                ?>
+                <div class="alert alert-danger">
+                    <strong>Sure !</strong> to remove the following record ?
+                </div>
+                <?php
+            }
+            ?>
+        </div>
 
-    <div class="container">
-        <p>
+        <div class="clearfix"></div>
+
+        <div class="container">
+
             <?php
             if(isset($_GET['delete_id']))
             {
+                ?>
+                <table class='table table-bordered'>
+                    <tr>
+                        <th>#</th>
+                        <th>Sumber Penghasilan</th>
+                        <th>Tanggal Penghasilan</th>
+                        <th>Nominal Penghasilan</th>
+                    </tr>
+                    <?php
+                    $stmt = $penghasilan->db->prepare("SELECT * FROM penghasilan WHERE penghasilanId=:id");
+                    $stmt->execute(array(":id"=>$_GET['delete_id']));
+                    while($row=$stmt->fetch(PDO::FETCH_BOTH))
+                    {
+                        ?>
+                        <tr>
+                            <td><?php print($row['penghasilanId']); ?></td>
+                            <td><?php print($row['sumberPghs']); ?></td>
+                            <td><?php print($row['tglPghs']); ?></td>
+                            <td align="right"><?php print("Rp ".$row['nominalPghs']); ?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </table>
+                <?php
+            }
             ?>
-        <form method="post">
-            <input type="hidden" name="id" value="<?php echo $row['Penghasilanid']; ?>" />
-            <button class="btn btn-large btn-primary" type="submit" name="btn-del"><i class="glyphicon glyphicon-trash"></i> &nbsp; YES</button>
-            <a href="view-penghasilan.php" class="btn btn-large btn-success"><i class="glyphicon glyphicon-backward"></i> &nbsp; NO</a>
-        </form>
-        <?php
-        }
-        else
-        {
-            ?>
-            <a href="view-penghasilan.php" class="btn btn-large btn-success"><i class="glyphicon glyphicon-backward"></i> &nbsp; Back to index</a>
+        </div>
+
+        <div class="container">
+            <p>
+                <?php
+                if(isset($_GET['delete_id']))
+                {
+                ?>
+            <form method="post">
+                <input type="hidden" name="id" value="<?php echo $row['Penghasilanid']; ?>" />
+                <button class="btn btn-large btn-primary" type="submit" name="btn-del"><i class="glyphicon glyphicon-trash"></i> &nbsp; YES</button>
+                <a href="view-penghasilan.php" class="btn btn-large btn-success"><i class="glyphicon glyphicon-backward"></i> &nbsp; NO</a>
+            </form>
             <?php
-        }
-        ?>
-        </p>
+            }
+            else
+            {
+                ?>
+                <a href="view-penghasilan.php" class="btn btn-large btn-success"><i class="glyphicon glyphicon-backward"></i> &nbsp; Back to index</a>
+                <?php
+            }
+            ?>
+            </p>
+        </div>
     </div>
 <?php include_once 'views/footer.php'; ?>
